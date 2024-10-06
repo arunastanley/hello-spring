3fd9cdb37983b93558acc324039ba59fd59cb46e
arunastanley <arunapaulus@gmail.com>
2024-10-06 13:18:35 -0500

    Chap13: Conditionals in templates- Views

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,9 +1,13 @@
 package org.launchcode.hellospring.controllers;
 
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -27,9 +31,10 @@ public class HelloController {
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String sayHello = "Hello, " + name + "!";
+        model.addAttribute("greeting",sayHello);
+        return "hello";
     }
 
     // /hello/form
@@ -38,4 +43,15 @@ public class HelloController {
         return "form";
     }
 
+
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> namesList = new ArrayList<>();
+        namesList.add("Java");
+        namesList.add("JavaScript");
+        namesList.add("Python");
+        model.addAttribute("names",namesList);
+        return "hello-names";
+    }
 }
