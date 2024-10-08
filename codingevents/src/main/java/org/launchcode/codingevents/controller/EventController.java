4bc054ba4cbd7f684b85a5f18e3f9fa375f27431
arunastanley <arunapaulus@gmail.com>
2024-10-08 14:17:35 -0500

    Chap13: ThymeLeaf forms:Create and Render a Form, bootstrap

--- codingevents/src/main/java/org/launchcode/codingevents/controller/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controller/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +14,14 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> eventList = new ArrayList<>();
+
     @GetMapping
-    public String events(Model model){
-        List<String> eventList = new ArrayList<>();
-        eventList.add("Load");
-        eventList.add("submit");
-        eventList.add("click");
+    public String displayEvents(Model model){
+//        List<String> eventList = new ArrayList<>();
+//        eventList.add("Load");
+//        eventList.add("submit");
+//        eventList.add("click");
         model.addAttribute("eventList",eventList);
         return "events/index";
     }
@@ -25,10 +29,14 @@ public class EventController {
 //    /events/create
     @GetMapping("create")
     public String createEventForm(){
-
         return "events/create";
     }
 
+    @PostMapping("create")
+    public String submitForm(@RequestParam String name){
+        eventList.add(name);
+        return "redirect:/events";
+    }
 
 
 }
